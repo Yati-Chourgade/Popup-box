65d71d7548bce8268914da642f9ccca4a077d433
Yati-Chourgade <90545052+Yati-Chourgade@users.noreply.github.com>
2022-07-15 22:08:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var check=false;
+
+    $('.clicks').on('click',function(){
+        if(check==false)
+        {
+            check=true;
+            $('#box').addClass('show');
+        }
+        else{
+            check=false;
+            $('#box').removeClass('show');
+        }
+    })
+
+    $('.submission').on('click',function(){
+        $('#box').removeClass('show');
+        check=false;
+    });
+
+    $('.close').on('click',function(){
+        $('#box').removeClass('show');
+        check=false;
+    });
+});
+
+
+
+
+
